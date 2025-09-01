6eb775bc14bbf5d2fbd286550733069b633dd8cf
spectre <peternjugunaw25@gmail.com>
2025-09-01 11:31:35 +0300

    fullstackjuly

---
+++ javascript/index.js
@@ -0,0 +1,65 @@
+/* Javascript is a lightweight scripting language that is used in the 
+web to develop login and add functionalities 
+to elements in the website.
+
+javascript was originally called ECMA script >>this was in 1995 ,but it kept on changing
+ based in useSyncExternalStore,compilers 
+flexibility ...until it was coined JavaScript.ES6 
+ javasvript in the web is categorised into {
+    fundamentals of js{
+        frontend:data types,functions,objects,arrays,loops,conditionals,asynchronous js ,DOM
+        backend:nodejs,express js
+    }
+ }          
+ frameworks{
+     react{library that uses the react router to navigate between pages}
+     ,angular{framework for building single-page applications},
+     vue{progressive framework for building user interfaces}
+     ,next{framework for building server-rendered React applications}>>fullstack webapps
+ }
+how do we use js in the browser{
+    firstly to see if js is running with the correct given logic we use node js
+    then we can use the browser's developer tools to debug and test our 
+    code in the console tab.
+}
+js can be added to the html in three ways{
+    inline js{<script>alert("Hello World!");</script>}
+    internal js{<script>console.log("Hello World!");</script>}
+    external js{<script src="script.js"></script>
+
+    }
+
+
+    Applications of js{
+        1. Client-side validation
+        2. Dynamic content updates
+        3. Event handling
+        4. AJAX requests
+        5. Animations and effects
+        6. Form validation
+        7. Mobile development
+        8. IoT 
+    }
+
+    variables>>this is a placeholder.storage for values/data types that are meant to be processed logically
+    {
+        thay are three in definition {
+            var>this allows room for declaration of variables but inside a function (this is function scoped and can be re-declared and updated)
+            const>this is block scoped and cannot be re-declared or updated age(age)
+            let>this is block scoped and can be updated but not re-declared
+        }
+    }
+
+    Data types in js{
+        1. Primitive types
+            - String
+            - Number
+            - Boolean
+            - Null
+            - Undefined
+            - Symbol
+        2. Reference types
+            - Object
+            - Array
+            - Function
+    } */
---
+++ javascript/practicals.js
@@ -0,0 +1,25 @@
+/* var redefining and redeclaring a new value for firstName
+ */
+var firstName="lilian"
+console.log(firstName);
+/* var firstName="derrick"
+console.log(firstName); */
+
+/* we cannot redeclared this variable name but we can update it 
+we update by calling the variable name and its nes paramenter/value and not \
+using the let keyword again  */
+let secondName="Mwangi"
+console.log(secondName);
+/*  secondName="Kenei"///update
+console.log(secondName); */
+
+const sage=35
+console.log(sage);
+//sage=36//we cannot update or redeclare this variable
+let age=36//we dont  allow variable declaration without a name >>this can result to undefined variables 
+console.log(age);
+thirdName=null;
+console.log(thirdName);
+
+/* combining all this using concatenation(+) */
+console.log("My name is"+" "+firstName+" "+secondName+",I am "+age+" years old");
